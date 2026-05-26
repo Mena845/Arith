7d69aaffd0d9651a2f3ae18e61e812f3602b050e
Mena845 <tsikiniainatsilavina@gmail.com>
2026-05-26 15:15:46 +0300

    add new condition

--- src/main/java/hei/school/arith/service/ArithmeticService.java
+++ src/main/java/hei/school/arith/service/ArithmeticService.java
@@ -6,21 +6,29 @@ import org.springframework.stereotype.Service;
 public class ArithmeticService {
     public int add(int a, int b) {
         if(a < 0 || b < 0) {
-            throw new IllegalArgumentException("Negative numbers are not allowed");
+            throw new IllegalArgumentException("Negative numbers are not allowed for addition");
         }
         return a + b;
     }
 
     public int substract(int a, int b) {
+        if(a < b) {
+            throw new IllegalArgumentException("a must be greater than or equal to b");
+        }
         return a - b;
     }
     public int multiply(int a, int b) {
-        return a * b;
+       if(a == 0 || b ==0) {
+           throw new IllegalArgumentException("Negative numbers are not allowed");
+       }
     }
 
     public int divide(int a, int b) {
         if (b == 0) {
             throw new IllegalArgumentException("Cannot divide by zero");
+        }
+        if (a <0 || b<0) {
+            throw new IllegalArgumentException("Negative numbers are not allowed for division");
         }
         return a / b;
     }
