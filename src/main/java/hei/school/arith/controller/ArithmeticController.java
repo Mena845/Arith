8aac879b98c4e3971deb8bc1ea253fbd147e18c5
Mena845 <tsikiniainatsilavina@gmail.com>
2026-05-26 14:24:34 +0300

    Add methode to added

--- src/main/java/hei/school/arith/controller/ArithmeticController.java
+++ src/main/java/hei/school/arith/controller/ArithmeticController.java
@@ -1,4 +1,14 @@
 package hei.school.arith.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 public class ArithmeticController {
+    @GetMapping("/add")
+    public int add(
+            @RequestParam int a,
+            @RequestParam int b
+    ) {
+        return a + b;
+    }
 }
