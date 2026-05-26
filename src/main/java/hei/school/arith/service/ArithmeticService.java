d1678ec6ed39288a2aa55d523631f13204b818a8
Mena845 <tsikiniainatsilavina@gmail.com>
2026-05-26 15:17:55 +0300

    fixed missing return statement

--- src/main/java/hei/school/arith/service/ArithmeticService.java
+++ src/main/java/hei/school/arith/service/ArithmeticService.java
@@ -18,9 +18,10 @@ public class ArithmeticService {
         return a - b;
     }
     public int multiply(int a, int b) {
-       if(a == 0 || b ==0) {
-           throw new IllegalArgumentException("Negative numbers are not allowed");
-       }
+        if (a == 0 || b == 0) {
+            throw new IllegalArgumentException("Negative numbers are not allowed");
+        }
+        return a * b;
     }
 
     public int divide(int a, int b) {
