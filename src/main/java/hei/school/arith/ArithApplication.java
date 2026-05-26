52e567404dc204340ff3bb8ef72dccce6eb84f87
Mena845 <tsikiniainatsilavina@gmail.com>
2026-05-26 14:15:35 +0300

    Add dependece

---
+++ src/main/java/hei/school/arith/ArithApplication.java
@@ -0,0 +1,13 @@
+package hei.school.arith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArithApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArithApplication.class, args);
+    }
+
+}
