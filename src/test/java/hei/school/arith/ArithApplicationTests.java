556bf957dec43c3e9fbc2c846986d3f8a471eb40
Mena845 <tsikiniainatsilavina@gmail.com>
2026-05-29 09:03:16 +0300

    Importaion of static Assertion

--- src/test/java/hei/school/arith/ArithApplicationTests.java
+++ src/test/java/hei/school/arith/ArithApplicationTests.java
@@ -1,37 +1,37 @@
 package hei.school.arith;
 
 import hei.school.arith.service.ArithmeticService;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest
 class ArithApplicationTests {
+    //    asaina instancieny spring
     private final ArithmeticService arithmeticService = new ArithmeticService();
-
     @Test
     void testAdd() {
-        long result = arithmeticService.add(50, 50);
-        Assertions.assertEquals(3, result);
+        long result = arithmeticService.add(50000000000L, 5000000000L);
+        assertEquals(3, result);
     }
 
 
     @Test
     void testSubtract() {
         long result = arithmeticService.substract(10 , 7);
-        Assertions.assertEquals(3, result);
+        assertEquals(3, result);
     }
 
     @Test
     void testMultiply() {
         long result = arithmeticService.multiply(10, 7);
-        Assertions.assertEquals(70, result);
+        assertEquals(70, result);
     }
 
     @Test
     void testDivide() {
         long result = arithmeticService.divide(10, 2);
-        Assertions.assertEquals(5, result);
+        assertEquals(5, result);
     }
 }
