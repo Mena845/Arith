5c0271b40b186b9d25e20323f8c5f6a1c74418a2
Mena845 <tsikiniainatsilavina@gmail.com>
2026-05-29 08:49:37 +0300

    fix groupId on pom.xml

--- src/test/java/hei/school/arith/ArithApplicationTests.java
+++ src/test/java/hei/school/arith/ArithApplicationTests.java
@@ -14,26 +14,26 @@ class ArithApplicationTests {
 
     @Test
     void testAdd() {
-        double result = arithmeticService.add(1, 2);
+        long result = arithmeticService.add(50, 50);
         Assertions.assertEquals(3, result);
     }
 
 
     @Test
     void testSubtract() {
-        double result = arithmeticService.substract(10 , 7);
+        long result = arithmeticService.substract(10 , 7);
         Assertions.assertEquals(3, result);
     }
 
     @Test
     void testMultiply() {
-        double result = arithmeticService.multiply(10, 7);
+        long result = arithmeticService.multiply(10, 7);
         Assertions.assertEquals(70, result);
     }
 
     @Test
     void testDivide() {
-        double result = arithmeticService.divide(10, 2);
+        long result = arithmeticService.divide(10, 2);
         Assertions.assertEquals(5, result);
     }
 }
